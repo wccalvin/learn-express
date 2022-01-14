9567c04ce4fb62ae1cd8aae4cfb674eeaa17eef8
wccalvin <clayton.calvin@gmail.com>
2022-01-14 11:53:41 -0500

    lesson: example to setup app listening

---
+++ myapp/01-setup-app-listen/app.js
@@ -0,0 +1,11 @@
+// import express library
+const express = require('express');
+// instantiate the app
+const app = express();
+// setup port number
+const PORT = process.env.PORT || 3001;
+
+// invoke app listen method
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
