8f5a25230274fb5cb1dcb1eba128d8e91e6cac9d
wccalvin <clayton.calvin@gmail.com>
2022-02-22 11:53:06 -0500

    reorg vars

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,8 +1,8 @@
 const { response } = require("express");
 const express = require("express");
 
-const port = 3000;
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello, there!</h1>");
