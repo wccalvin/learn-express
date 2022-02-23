68615988e8f4b03d2d2b382dbe1d706e1d1906e2
wccalvin <clayton.calvin@gmail.com>
2022-02-23 12:58:34 -0500

    feat: use pug templates

--- flashcards/app.js
+++ flashcards/app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello, there!</h1>");
+  res.render("index");
 });
 app.get("/hello", (req, res) => {
   res.send("<h1>Hello, JS dev!</h1>");
