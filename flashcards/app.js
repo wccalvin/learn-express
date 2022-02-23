8c7f6a918c88e4ce76677bb573af7c624432b5c7
wccalvin <clayton.calvin@gmail.com>
2022-02-23 15:13:06 -0500

    feat: interpolate in pug variables

--- flashcards/app.js
+++ flashcards/app.js
@@ -5,12 +5,16 @@ const app = express();
 const port = 3000;
 
 app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/hello", (req, res) => {
-  res.send("<h1>Hello, JS dev!</h1>");
+app.get("/cards", (req, res) => {
+  res.render("card", {
+    prompt: "Who is burried in Grant's tomb?",
+    hint: "Think about whose tomb it is.",
+  });
 });
 
 app.listen(port, () => {
