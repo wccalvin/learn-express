5e26af40f5945836b948e9a506afdcff0250b724
wccalvin <clayton.calvin@gmail.com>
2022-02-24 11:16:07 -0500

    feat: use body-parser to get info from form

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,22 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
+app.set("views", `${__dirname}/views`);
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/hello", (req, res) => {
+  // eslint-disable-next-line quotes
   res.render("hello");
 });
 
 app.post("/hello", (req, res) => {
-  console.dir(req.body);
+  console.log(req.body);
   res.render("hello");
 });
 
