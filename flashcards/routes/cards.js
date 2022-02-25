cc57861ee2941969793c28b8e6b377b88cd885e2
wccalvin <clayton.calvin@gmail.com>
2022-02-25 15:35:25 -0500

    provide test data

--- flashcards/routes/cards.js
+++ flashcards/routes/cards.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const { data } = require("../data/flashcardData.json");
+const { cards } = data;
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("card", {
-    prompt: "Who is the creator of python?",
-    hint: "Dutch Programmer.",
-  });
+router.get("/:id", (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+  const templateData = { text };
+  if (side === "question") {
+    templateData.hint = hint;
+  }
+  res.render("card", templateData);
 });
 
 module.exports = router;
